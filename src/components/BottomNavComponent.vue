11bf071f652d8c987e136ad27762998b4a65997e
owner6 <owner1147@gmail.com>
2024-06-14 23:33:06 +0300

    implementation of a new structure of components

---
+++ src/components/BottomNavComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="bottom-nav">
+    <search-component @search="handleSearch"></search-component>
+  </nav>
+</template>
+
+<script>
+import SearchComponent from "./UI/SearchComponent.vue";
+
+export default {
+  name: "BottomNavComponent",
+  components: {
+    SearchComponent,
+  },
+  methods: {
+    handleSearch(query) {
+      // logic обробки search
+      console.log(query);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/MainHeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="main-header">
+    <top-nav-component />
+    <bottom-nav-component />
+  </header>
+</template>
+
+<script>
+import TopNavComponent from "./TopNavComponent.vue";
+import BottomNavComponent from "./BottomNavComponent.vue";
+
+export default {
+  name: "MainHeaderComponent",
+  components: {
+    TopNavComponent,
+    BottomNavComponent,
+  },
+};
+</script>
+
+<style scoped>
+.main-header {
+  width: 100%;
+}
+</style>
