f20a18ddb48efce7140def09c489fe1cf92dc70f
owner6 <owner1147@gmail.com>
2024-06-26 23:51:57 +0300

    setting paddings listings-header

--- src/components/TopNavComponent.vue
+++ src/components/TopNavComponent.vue
@@ -53,8 +53,8 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
 .listings-header {
-  background-color: $light-color;
-  padding: 0;
+  background-color: #f4f4f4;
+  padding: 0px 75px 0px 75px;
 
   .navbar {
     background-color: $dark-color;
@@ -63,7 +63,6 @@ export default {
       display: flex;
       justify-content: space-between;
       align-items: center;
-      height: 70px;
 
       .navbar-start,
       .navbar-end {
