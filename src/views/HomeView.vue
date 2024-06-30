ed0105b856195ff01178eb244406c29b39864fed
Owner <owner1147@gmail.com>
2024-06-30 20:46:33 +0300

    location and styling of elements in home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="home-view">
-    <section class="hero">
-      <NavCategories class="nav-categories" />
-      <div class="hero-content">
+    <section class="section-left">
+      <div class="section-content">
         <h1>Everything can be bought and sold here!</h1>
         <div class="search">
           <search-component @search="handleSearch"></search-component>
         </div>
       </div>
     </section>
+    <section class="section-right">
+      <img src="@/assets/images/image-section-right.jpg" />
+    </section>
   </div>
 </template>
 
 <script>
 import SearchComponent from "../components/UI/SearchPanel.vue";
-import NavCategories from "../components/NavCategories.vue";
 
 export default {
   name: "HomeView",
   components: {
     SearchComponent,
-    NavCategories,
   },
 };
 </script>
@@ -29,24 +29,30 @@ export default {
 @import "@/assets/scss/main.scss";
 
 .home-view {
-  .hero {
-    position: relative;
-    background-color: $extra-color-LightGreenPastel;
-    color: $text-color;
-    text-align: center;
-    padding: 150px 235px 150px 45px;
+  display: flex;
+  padding: 0 27px;
+  height: 626px;
+
+  .section-right {
+    flex: 1;
     border-radius: 24px;
-    height: 626px;
+  }
 
-    h1 {
-      font-size: $H1;
-      font-weight: 400;
-    }
+  .section-left {
+    flex: 1;
+    background-color: $extra-color-LightGreenPastel;
+    border-radius: 24px;
+    padding: 150px 45px;
+    text-align: left;
 
-    .hero-content {
+    .section-content {
       width: 410px;
       height: 310px;
-      text-align: start;
+
+      h1 {
+        font-size: $H1;
+        font-weight: 400;
+      }
 
       .search {
         margin-top: 16px;
@@ -66,39 +72,16 @@ export default {
         }
       }
     }
-
-    .nav-categories {
-      position: absolute;
-      top: 0;
-      width: 100%;
-    }
   }
 
-  .features {
+  .section-right {
     display: flex;
-    justify-content: space-around;
-    padding: $spacing-unit * 4;
-    background-color: $extra-color-LightGreenPastel;
+    align-items: start;
+    justify-content: center;
 
-    .feature {
-      text-align: center;
-      max-width: 250px;
-
-      h1 {
-        font-size: $H1;
-        color: $text-color;
-      }
-
-      h2 {
-        font-size: 24px;
-        margin-bottom: $spacing-unit;
-        color: $text-color;
-      }
-
-      p {
-        font-size: 16px;
-        color: $text-color;
-      }
+    img {
+      max-width: 626px;
+      border-radius: 24px;
     }
   }
 }
