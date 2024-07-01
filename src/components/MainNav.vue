245b6d03a81d5de48766f35bf67cdc7f38707307
Owner <owner1147@gmail.com>
2024-07-01 12:12:14 +0300

    addition of the first component

---
+++ src/components/MainNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="main-header">
+    <top-nav-component />
+  </header>
+</template>
+
+<script>
+import TopNavComponent from "./TopNav.vue";
+
+export default {
+  name: "MainHeaderComponent",
+  components: {
+    TopNavComponent,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/main.scss";
+
+.main-header {
+  width: 100%;
+}
+</style>
