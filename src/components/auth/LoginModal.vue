0228eddff7ade0b8e575f4f47d35bf699bce0744
owner <owner1147@gmail.com>
2024-08-16 09:58:45 +0300

    login

--- src/components/auth/LoginModal.vue
+++ src/components/auth/LoginModal.vue
@@ -4,6 +4,7 @@
       <span class="close" @click="close">&times;</span>
       <h2 v-if="isLoginForm">Login</h2>
       <h2 v-else>Register</h2>
+
       <form v-if="isLoginForm" @submit.prevent="login">
         <div>
           <label for="email">Email:</label>
@@ -13,15 +14,12 @@
           <label for="password">Password:</label>
           <input type="password" v-model="password" required />
         </div>
-        <div>
-          <label for="confirmPassword">Confirm Password:</label>
-          <input type="password" v-model="confirmPassword" required />
-        </div>
         <button type="submit">Login</button>
         <p class="toggle" @click="toggleForm">
           Don't have an account? Register
         </p>
       </form>
+
       <register-form v-else @close="close" @toggle="toggleForm" />
     </div>
   </div>
@@ -43,12 +41,10 @@ export default {
   data() {
     return {
       email: "",
-      phone: "",
-      lastname: "",
-      firstname: "",
       password: "",
       confirmPassword: "",
       isLoginForm: true,
+      API_URL: process.env.VUE_APP_API_URL || "http://localhost:3000",
     };
   },
 
@@ -95,53 +91,6 @@ export default {
       } catch (error) {
         console.error("Login error", error);
       }
-    },
-
-    async register() {
-      try {
-        const response = await axios.post(
-          `${this.API_URL}/auth/registration`,
-          {
-            email: this.email,
-            phone: this.phone,
-            lastname: this.lastname,
-            firstname: this.firstname,
-            password: this.password,
-          },
-          {
-            headers: { "Content-Type": "application/json" },
-          }
-        );
-
-        const token = response.data.token;
-
-        localStorage.setItem("authToken", token);
-
-        this.user = response.data.user;
-
-        this.isLoading = false;
-
-        await this.login();
-
-        router.push({ path: "/mypage" });
-
-        window.location.reload();
-
-        this.showAuthForm = false;
-      } catch (error) {
-        console.error("Registration error", error);
-      }
-    },
-
-    validateRegistration() {
-      if (
-        !this.email ||
-        !this.password ||
-        (this.isRegistering && this.password !== this.confirmPassword)
-      ) {
-        return false;
-      }
-      return true;
     },
 
     close() {
