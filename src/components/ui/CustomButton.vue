d97c3b346b3422d016cdaffc9430b8f83619eb95
owner <owner1147@gmail.com>
2025-03-06 21:20:22 +0200

    Complete Button Styles

--- src/components/ui/CustomButton.vue
+++ src/components/ui/CustomButton.vue
@@ -19,7 +19,7 @@ const props = defineProps({
 
 const emits = defineEmits(['click']);
 
-// Обробник кліку
+// Обробник click
 const handleClick = () => {
   if (props.action) {
     props.action(); // Викликаємо передану функцію
@@ -40,21 +40,122 @@ const buttonClass = computed(() => {
 <style lang="scss">
 @import '@/assets/scss/main.scss';
 
-.btn-tertiary {
+// Common button properties
+%button-base {
+  font-weight: 500;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+}
+
+// Primary Button
+.btn-primary {
+  @extend %button-base;
   font-size: $font-size-xxs;
+  background-color: map-get($colors, peach);
+  color: map-get($colors, black);
+  border: none;
+  border-radius: 7px;
+  padding: 12px 24px;
+
+  &:hover {
+    background-color: lighten(map-get($colors, peach), 10%);
+  }
+
+  &:active,
+  &.pressed {
+    background-color: (map-get($colors, peach), 5%);
+  }
+
+  &:disabled {
+    background-color: map-get($colors, softgray);
+    color: (map-get($colors, softgray), 30%);
+  }
+}
+
+// Secondary Button
+.btn-secondary {
+  @extend %button-base;
+  font-size: $font-size-xxs;
+  background-color: transparent;
+  color: map-get($colors, black);
+  border: solid 1px map-get($colors, black);
+  border-radius: 7px;
+  padding: 12px 24px;
+
+  &:hover {
+    color: map-get($colors, peach);
+    border-color: map-get($colors, peach);
+  }
+
+  &:active,
+  &.pressed {
+    color: map-get($colors, peach);
+    border-color: map-get($colors, peach);
+  }
+
+  &:disabled {
+    color: map-get($colors, softgray);
+    border-color: map-get($colors, softgray);
+  }
+}
+
+// Tertiary Button
+.btn-tertiary {
+  @extend %button-base;
+  font-size: $font-size-xxs;
+  background-color: transparent;
+  border: none;
+  padding: 12px 24px;
 
   &:hover {
     color: map-get($colors, peach);
   }
+
+  &:active,
+  &.pressed {
+    color: map-get($colors, peach);
+  }
+
+  &:disabled {
+    color: map-get($colors, softgray);
+  }
 }
 
-.btn-secondary {
-  font-size: $font-size-xxs;
-  border: solid 1px;
-  border-radius: 7px;
-  padding: 12px 24px 12px 24px;
+// Arrow Button
+.btn-arrow {
+  @extend %button-base;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  background-color: map-get($colors, black);
+  color: white;
+  border: none;
+  border-radius: 50%;
+  padding: 0;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+
   &:hover {
-    color: map-get($colors, peach);
+    background-color: map-get($colors, peach);
+  }
+
+  &:active,
+  &.pressed {
+    background-color: (map-get($colors, peach), 5%);
+  }
+
+  &:disabled {
+    background-color: map-get($colors, softgray);
+    color: (map-get($colors, softgray), 20%);
   }
 }
 </style>
